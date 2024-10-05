cafa2a8eedd543d2380cb51cce4cd1ec0b4929c3
ilyxabatko <ilyxabatko@inbox.ru>
2024-10-05 17:44:10 +0400

    upd: adds a macro for transaction decoding

--- decoders/system-program-decoder/src/instructions/mod.rs
+++ decoders/system-program-decoder/src/instructions/mod.rs
@@ -1,9 +1,9 @@
+use crate::SystemProgramDecoder;
 use carbon_core::{
     deserialize::CarbonDeserialize,
     instruction::{DecodedInstruction, InstructionDecoder},
 };
-
-use crate::SystemProgramDecoder;
+use carbon_macros::try_decode_instructions;
 
 pub mod advance_nonce_account;
 pub mod allocate;
@@ -52,114 +52,20 @@ impl InstructionDecoder for SystemProgramDecoder {
         &self,
         instruction: solana_sdk::instruction::Instruction,
     ) -> Option<DecodedInstruction<Self::InstructionType>> {
-        if let Some(decoded_instruction) =
-            create_account::CreateAccount::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::CreateAccount(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) = assign::Assign::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::Assign(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            transfer::Transfer::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::Transfer(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            create_account_with_seed::CreateAccountWithSeed::deserialize(
-                instruction.data.as_slice(),
-            )
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::CreateAccountWithSeed(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            advance_nonce_account::AdvanceNonceAccount::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::AdvanceNonceAccount(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            withdraw_nonce_account::WithdrawNonceAccount::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::WithdrawNonceAccount(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            initialize_nonce_account::InitializeNonceAccount::deserialize(
-                instruction.data.as_slice(),
-            )
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::InitializeNonceAccount(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            authorize_nonce_account::AuthorizeNonceAccount::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::AuthorizeNonceAccount(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            allocate::Allocate::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::Allocate(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            allocate_with_seed::AllocateWithSeed::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::AllocateWithSeed(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            assign_with_seed::AssignWithSeed::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::AssignWithSeed(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            transfer_with_seed::TransferWithSeed::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::TransferWithSeed(decoded_instruction),
-            });
-        }
-        if let Some(decoded_instruction) =
-            upgrade_nonce_account::UpgradeNonceAccount::deserialize(instruction.data.as_slice())
-        {
-            return Some(carbon_core::instruction::DecodedInstruction {
-                program_id: instruction.program_id,
-                data: SystemProgramInstruction::UpgradeNonceAccount(decoded_instruction),
-            });
-        }
-
-        None
+        try_decode_instructions!(instruction,
+            SystemProgramInstruction::CreateAccount => create_account::CreateAccount,
+            SystemProgramInstruction::Assign => assign::Assign,
+            SystemProgramInstruction::Transfer => transfer::Transfer,
+            SystemProgramInstruction::CreateAccountWithSeed => create_account_with_seed::CreateAccountWithSeed,
+            SystemProgramInstruction::AdvanceNonceAccount => advance_nonce_account::AdvanceNonceAccount,
+            SystemProgramInstruction::WithdrawNonceAccount => withdraw_nonce_account::WithdrawNonceAccount,
+            SystemProgramInstruction::InitializeNonceAccount => initialize_nonce_account::InitializeNonceAccount,
+            SystemProgramInstruction::AuthorizeNonceAccount => authorize_nonce_account::AuthorizeNonceAccount,
+            SystemProgramInstruction::Allocate => allocate::Allocate,
+            SystemProgramInstruction::AllocateWithSeed => allocate_with_seed::AllocateWithSeed,
+            SystemProgramInstruction::AssignWithSeed => assign_with_seed::AssignWithSeed,
+            SystemProgramInstruction::TransferWithSeed => transfer_with_seed::TransferWithSeed,
+            SystemProgramInstruction::UpgradeNonceAccount => upgrade_nonce_account::UpgradeNonceAccount,
+        )
     }
 }
