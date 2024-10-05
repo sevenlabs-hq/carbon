5a3b69edc590d893caed45160d32016c7c038e79
ilyxabatko <ilyxabatko@inbox.ru>
2024-10-05 17:22:02 +0400

    progress

---
+++ decoders/system-program-decoder/src/instructions/transfer.rs
@@ -0,0 +1,33 @@
+use carbon_core::borsh;
+use carbon_core::deserialize::{ArrangeAccounts, CarbonDeserialize};
+use carbon_proc_macros::CarbonDeserialize;
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+#[carbon(discriminator = "0x02")]
+pub struct Transfer {
+    pub onwer: solana_sdk::pubkey::Pubkey,
+}
+
+pub struct TransferAccounts {
+    pub funding_account: solana_sdk::pubkey::Pubkey,
+    pub recipient_account: solana_sdk::pubkey::Pubkey,
+}
+
+impl ArrangeAccounts for Transfer {
+    type ArrangedAccounts = TransferAccounts;
+
+    fn arrange_accounts(
+        &self,
+        accounts: Vec<solana_sdk::pubkey::Pubkey>,
+    ) -> Option<Self::ArrangedAccounts> {
+        let funding_account = accounts.get(0)?;
+        let recipient_account = accounts.get(1)?;
+
+        Some(TransferAccounts {
+            funding_account: *funding_account,
+            recipient_account: *recipient_account,
+        })
+    }
+}
