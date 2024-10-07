3323da0ae695f156508006762de0a65a413376ee
Nikola Draskovic <draskovic26z@gmail.com>
2024-10-07 11:50:21 +0200

    initial

---
+++ examples/accounts-test/src/pump_decoder/instructions/set_params_event.rs
@@ -0,0 +1,17 @@
+
+use carbon_core::deserialize::CarbonDeserialize;
+use carbon_proc_macros::CarbonDeserialize;
+use carbon_core::borsh;
+use super::super::types::*;
+
+
+#[derive(CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash)]
+#[carbon(discriminator = "0xe445a52e51cb9a1ddfc39ff63e308f83")]
+pub struct SetParamsEvent{
+    pub fee_recipient: solana_sdk::pubkey::Pubkey,
+    pub initial_virtual_token_reserves: u64,
+    pub initial_virtual_sol_reserves: u64,
+    pub initial_real_token_reserves: u64,
+    pub token_total_supply: u64,
+    pub fee_basis_points: u64,
+}
