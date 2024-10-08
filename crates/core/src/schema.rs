3c39005d5f72505783583abcfbb992c9890c0be9
Nikola Draskovic <draskovic26z@gmail.com>
2024-10-08 12:15:12 +0200

    adds accounts to tx side

--- crates/core/src/schema.rs
+++ crates/core/src/schema.rs
@@ -1,6 +1,7 @@
 use crate::collection::InstructionDecoderCollection;
 use crate::instruction::DecodedInstruction;
 use serde::de::DeserializeOwned;
+use solana_sdk::instruction::AccountMeta;
 use solana_sdk::pubkey::Pubkey;
 use std::collections::HashMap;
 
@@ -37,8 +38,11 @@ impl<T: InstructionDecoderCollection> TransactionSchema<T> {
         serde_json::from_value::<U>(serde_json::to_value(self.match_nodes(instructions)).ok()?).ok()
     }
 
-    pub fn match_nodes(&self, instructions: &[ParsedInstruction<T>]) -> Option<HashMap<String, T>> {
-        let mut output = HashMap::<String, T>::new();
+    pub fn match_nodes(
+        &self,
+        instructions: &[ParsedInstruction<T>],
+    ) -> Option<HashMap<String, (T, Vec<AccountMeta>)>> {
+        let mut output = HashMap::<String, (T, Vec<AccountMeta>)>::new();
 
         let current_index = 0;
         let current_instruction = instructions.get(current_index)?;
@@ -60,7 +64,10 @@ impl<T: InstructionDecoderCollection> TransactionSchema<T> {
                     } else {
                         output.insert(
                             ix.name.clone(),
-                            current_instruction.instruction.data.clone(),
+                            (
+                                current_instruction.instruction.data.clone(),
+                                current_instruction.instruction.accounts.clone(),
+                            ),
                         );
                     }
 
@@ -88,10 +95,12 @@ impl<T: InstructionDecoderCollection> TransactionSchema<T> {
     }
 }
 
-pub fn merge_hashmaps<K, V>(a: HashMap<K, V>, b: HashMap<K, V>) -> HashMap<K, V>
+pub fn merge_hashmaps<K, V>(
+    a: HashMap<K, (V, Vec<AccountMeta>)>,
+    b: HashMap<K, (V, Vec<AccountMeta>)>,
+) -> HashMap<K, (V, Vec<AccountMeta>)>
 where
     K: std::cmp::Eq + std::hash::Hash,
-    V: std::cmp::Eq + std::hash::Hash,
 {
     let mut output = a;
     for (key, value) in b {
