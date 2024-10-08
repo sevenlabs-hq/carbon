418ec30f089c5f521139d17ba548d908a98137b4
Nikola Draskovic <draskovic26z@gmail.com>
2024-10-08 11:34:42 +0200

    changes

--- examples/accounts-test/src/pump_decoder/instructions/mod.rs
+++ examples/accounts-test/src/pump_decoder/instructions/mod.rs
@@ -1,21 +1,28 @@
-
 use carbon_core::deserialize::CarbonDeserialize;
 use carbon_core::instruction::InstructionDecoder;
 
-
 use super::PumpDecoder;
-pub mod initialize;
-pub mod set_params;
-pub mod create;
 pub mod buy;
-pub mod sell;
-pub mod withdraw;
-pub mod create_event;
-pub mod trade_event;
 pub mod complete_event;
+pub mod create;
+pub mod create_event;
+pub mod initialize;
+pub mod sell;
+pub mod set_params;
 pub mod set_params_event;
+pub mod trade_event;
+pub mod withdraw;
 
-#[derive(carbon_proc_macros::InstructionType, serde::Serialize, serde::Deserialize, PartialEq, Eq, Debug, Clone, Hash)]
+#[derive(
+    carbon_proc_macros::InstructionType,
+    serde::Serialize,
+    serde::Deserialize,
+    PartialEq,
+    Eq,
+    Debug,
+    Clone,
+    Hash,
+)]
 pub enum PumpInstruction {
     Initialize(initialize::Initialize),
     SetParams(set_params::SetParams),
@@ -36,19 +43,24 @@ impl InstructionDecoder for PumpDecoder {
         &self,
         instruction: solana_sdk::instruction::Instruction,
     ) -> Option<carbon_core::instruction::DecodedInstruction<Self::InstructionType>> {
-        if let Some(decoded_instruction) = initialize::Initialize::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            initialize::Initialize::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::Initialize(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = set_params::SetParams::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            set_params::SetParams::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::SetParams(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = create::Create::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) = create::Create::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::Create(decoded_instruction),
@@ -66,31 +78,41 @@ impl InstructionDecoder for PumpDecoder {
                 data: PumpInstruction::Sell(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = withdraw::Withdraw::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            withdraw::Withdraw::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::Withdraw(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = create_event::CreateEvent::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            create_event::CreateEvent::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::CreateEvent(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = trade_event::TradeEvent::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            trade_event::TradeEvent::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::TradeEvent(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = complete_event::CompleteEvent::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            complete_event::CompleteEvent::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::CompleteEvent(decoded_instruction),
             });
         }
-        if let Some(decoded_instruction) = set_params_event::SetParamsEvent::deserialize(instruction.data.as_slice()) {
+        if let Some(decoded_instruction) =
+            set_params_event::SetParamsEvent::deserialize(instruction.data.as_slice())
+        {
             return Some(carbon_core::instruction::DecodedInstruction {
                 program_id: instruction.program_id,
                 data: PumpInstruction::SetParamsEvent(decoded_instruction),
