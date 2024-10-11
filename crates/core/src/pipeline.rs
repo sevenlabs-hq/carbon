6b25118ce54785bb391cbdad29c0684f206f5511
ilyxabatko <ilyxabatko@inbox.ru>
2024-10-11 19:43:50 +0400

    progress

--- crates/core/src/pipeline.rs
+++ crates/core/src/pipeline.rs
@@ -1,5 +1,3 @@
-use serde::de::DeserializeOwned;
-
 use crate::{
     account::{AccountDecoder, AccountMetadata, AccountPipe, AccountPipes, DecodedAccount},
     account_deletion::{AccountDeletionPipe, AccountDeletionPipes},
@@ -15,9 +13,11 @@ use crate::{
     transaction::{TransactionPipe, TransactionPipes},
     transformers,
 };
+use serde::de::DeserializeOwned;
+use tokio_util::sync::CancellationToken;
 
 pub struct Pipeline {
-    pub datasource: Box<dyn Datasource>,
+    pub datasource: Box<dyn Datasource + Send + Sync>,
     pub account_pipes: Vec<Box<dyn AccountPipes>>,
     pub account_deletion_pipes: Vec<Box<dyn AccountDeletionPipes>>,
     pub instruction_pipes: Vec<Box<dyn for<'a> InstructionPipes<'a>>>,
@@ -37,7 +37,11 @@ impl Pipeline {
 
     pub async fn run(&mut self) -> CarbonResult<()> {
         let (update_sender, mut update_receiver) = tokio::sync::mpsc::unbounded_channel::<Update>();
-        let _abort_handle = self.datasource.consume(&update_sender).await?;
+        let cancellation_token = CancellationToken::new();
+
+        self.datasource
+            .consume(&update_sender, cancellation_token.clone())
+            .await?;
 
         if !self.account_pipes.is_empty()
             && !self
@@ -62,27 +66,31 @@ impl Pipeline {
         }
 
         loop {
-            match update_receiver.try_recv() {
-                Ok(update) => match self.process(update.clone()).await {
-                    Ok(_) => log::trace!("processed update"),
-                    Err(error) => log::error!("error processing update: {:?}", error),
-                },
-                Err(error) => match error {
-                    tokio::sync::mpsc::error::TryRecvError::Disconnected => {
-                        break;
+            tokio::select! {
+                _ = tokio::signal::ctrl_c() => {
+                    log::trace!("Shutdown signal received, sending cancellation requests and dropping the update sender...");
+                    cancellation_token.cancel();
+                    drop(update_sender);
+                    break;
+                }
+                recv_result = update_receiver.recv() => {
+                    match recv_result {
+                        Some(update) => {
+                            self.process(update).await?;
+                        }
+                        None => {
+                            log::trace!("Pipeline update sender has been closed.");
+                            break;
+                        }
                     }
-                    tokio::sync::mpsc::error::TryRecvError::Empty => {
-                        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-                        continue;
-                    }
-                },
+                }
             }
         }
 
         Ok(())
     }
 
-    pub async fn process(&mut self, update: Update) -> CarbonResult<()> {
+    async fn process(&mut self, update: Update) -> CarbonResult<()> {
         match update {
             Update::Account(account_update) => {
                 let account_metadata = AccountMetadata {
---
+++ libs/helius-rust-sdk/src/factory.rs
@@ -0,0 +1,96 @@
+use std::sync::Arc;
+
+use crate::client::Helius;
+use crate::config::Config;
+use crate::error::Result;
+use crate::rpc_client::RpcClient;
+use crate::types::Cluster;
+
+use reqwest::Client;
+
+/// A factory for creating instances of `Helius`
+///
+/// This factory allows for a centralized configuration and creation of `Helius` clients so work can be done across multiple clusters at the same time.
+/// Using a factory simplifies client code and enhances maintainability by ensuring that all `Helius` clients are configured consistently.
+pub struct HeliusFactory {
+    api_key: String,
+    client: Option<Client>,
+}
+
+impl HeliusFactory {
+    /// Creates a new `HeliusFactor` capable of producing `Helius` clients
+    ///
+    /// # Arguments
+    /// * `api_key` - The API key used for authenticating requests made by the `Helius` clients
+    ///
+    /// # Example
+    /// ```rust
+    /// use helius::HeliusFactory;
+    /// let factory = HeliusFactory::new("your_api_key_here");
+    /// ```
+    pub fn new(api_key: &str) -> Self {
+        HeliusFactory {
+            api_key: api_key.to_string(),
+            client: None,
+        }
+    }
+
+    /// Use your own reqwest client
+    ///
+    /// # Arguments
+    /// * `client` - a [`reqwest::Client`]
+    ///
+    /// # Example
+    /// ```rust
+    /// use helius::HeliusFactory;
+    /// use helius::types::Cluster;
+    /// let mut factory = HeliusFactory::new("your_api_key_here");
+    /// factory.with_client(reqwest::Client::new()).create(Cluster::Devnet).unwrap();
+    /// ```
+    pub fn with_client(&mut self, client: Client) -> &mut Self {
+        self.client = Some(client);
+        self
+    }
+
+    /// Provides a way to create multiple `Helius` clients in a thread-safe manner
+    ///
+    /// # Arguments
+    /// * `cluster` - The Solana cluster for which the `Helius` client will be configured
+    ///
+    /// # Returns
+    /// A `Result` wrapping a `Helius` client if successful
+    ///
+    /// # Errors
+    /// This method returns a `HeliusError` if the configuration or client instantiation fails, typically due to issues with network settings or the API key provided
+    ///
+    /// # Example
+    /// ```rust
+    /// use helius::types::*;
+    /// use helius::client::Helius;
+    /// use helius::factory::HeliusFactory;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let factory = HeliusFactory::new("your_api_key_here");
+    ///
+    ///     let helius_dev = factory.create(Cluster::Devnet).unwrap();
+    ///     // Perform devnet request: helius_dev.request_name().await;
+    ///     
+    ///     let helius_main = factory.create(Cluster::MainnetBeta).unwrap();
+    ///     // Perform mainnet request: helius_main.request_name().await;
+    /// }
+    /// ```
+    pub fn create(&self, cluster: Cluster) -> Result<Helius> {
+        let config: Arc<Config> = Arc::new(Config::new(&self.api_key, cluster)?);
+        let client: Client = self.client.clone().unwrap_or_default();
+        let rpc_client: Arc<RpcClient> = Arc::new(RpcClient::new(Arc::new(client.clone()), config.clone())?);
+
+        Ok(Helius {
+            config,
+            client,
+            rpc_client,
+            async_rpc_client: None,
+            ws_client: None,
+        })
+    }
+}
