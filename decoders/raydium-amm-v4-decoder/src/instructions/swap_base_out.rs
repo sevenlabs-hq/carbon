804cc9fba5826e984afec6c188eb9c9434d7297a
passingBy <cryptobs045@gmail.com>
2024-11-15 21:51:51 +0300

    feat: make a POC fix for raydium-amm-v4-decoder

--- decoders/raydium-amm-v4-decoder/src/instructions/swap_base_out.rs
+++ decoders/raydium-amm-v4-decoder/src/instructions/swap_base_out.rs
@@ -3,14 +3,8 @@ use carbon_core::{borsh, CarbonDeserialize};
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
-#[carbon(discriminator = "0xe445a52e51cb9a1d835c51b828654810")]
+#[carbon(discriminator = "0x0b")]
 pub struct SwapBaseOut {
-    pub log_type: u8,
-    pub max_amount_in: u64,
-    pub amount_out: u64,
-    pub direction: u64,
-    pub user_source: u64,
-    pub pool_coin: u64,
-    pub pool_pc: u64,
-    pub deduct_in: u64,
+    pub amount_in: u64,
+    pub minimum_amount_out: u64,
 }
