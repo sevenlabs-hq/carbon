a631f8c4a3b9228e97b97c7d4a8b2b46298302d9
passingBy <cryptobs045@gmail.com>
2024-11-18 18:45:29 +0300

    feat: replace discriminators for instructions

--- decoders/raydium-amm-v4-decoder/src/instructions/swap_base_out.rs
+++ decoders/raydium-amm-v4-decoder/src/instructions/swap_base_out.rs
@@ -5,6 +5,6 @@ use carbon_core::{borsh, CarbonDeserialize};
 )]
 #[carbon(discriminator = "0x0b")]
 pub struct SwapBaseOut {
-    pub amount_in: u64,
-    pub minimum_amount_out: u64,
+    pub max_amount_in: u64,
+    pub amount_out: u64,
 }
--- decoders/raydium-amm-v4-decoder/src/instructions/withdraw.rs
+++ decoders/raydium-amm-v4-decoder/src/instructions/withdraw.rs
@@ -3,16 +3,7 @@ use carbon_core::{borsh, CarbonDeserialize};
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
-#[carbon(discriminator = "0xe445a52e51cb9a1dc0f1c9d946965af7")]
+#[carbon(discriminator = "0x04")]
 pub struct Withdraw {
-    pub log_type: u8,
-    pub withdraw_lp: u64,
-    pub user_lp: u64,
-    pub pool_coin: u64,
-    pub pool_pc: u64,
-    pub pool_lp: u64,
-    pub calc_pnl_x: u128,
-    pub calc_pnl_y: u128,
-    pub out_coin: u64,
-    pub out_pc: u64,
+    pub amount: u64,
 }
