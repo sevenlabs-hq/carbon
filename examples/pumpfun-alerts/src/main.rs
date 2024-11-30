7f2fc706a2f62044d8fd4bcab597dd8d33431841
KellianDev <kdev.solana@gmail.com>
2024-11-30 13:37:33 +0400

    fix(helius-atlas-ws): use helius sdk 0.2.2 and add the datasource to the pumpfun example

--- examples/pumpfun-alerts/src/main.rs
+++ examples/pumpfun-alerts/src/main.rs
@@ -1,13 +1,13 @@
 use async_trait::async_trait;
 use carbon_core::{
-    error::CarbonResult,
-    instruction::{DecodedInstruction, InstructionMetadata, NestedInstruction},
-    metrics::MetricsCollection,
+    error::CarbonResult, instruction::InstructionProcessorInputType, metrics::MetricsCollection,
     processor::Processor,
 };
+use carbon_helius_atlas_ws_datasource::HeliusWebsocket;
 use carbon_pumpfun_decoder::{instructions::PumpfunInstruction, PumpfunDecoder};
 use carbon_yellowstone_grpc_datasource::YellowstoneGrpcGeyserClient;
-use solana_sdk::{native_token::LAMPORTS_PER_SOL, pubkey, pubkey::Pubkey};
+use solana_sdk::pubkey;
+use solana_sdk::{native_token::LAMPORTS_PER_SOL, pubkey::Pubkey};
 use std::{
     collections::{HashMap, HashSet},
     env,
@@ -20,11 +20,10 @@ use yellowstone_grpc_proto::geyser::{
 
 pub const PUMPFUN_PROGRAM_ID: Pubkey = pubkey!("6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P");
 
-#[tokio::main]
-pub async fn main() -> CarbonResult<()> {
-    env_logger::init();
-    dotenv::dotenv().ok();
-
+pub fn create_yellowstone_grpc_datasource(
+    grpc_url: String,
+    x_token: String,
+) -> YellowstoneGrpcGeyserClient {
     let account_filters: HashMap<String, SubscribeRequestFilterAccounts> = HashMap::new();
 
     let transaction_filter = SubscribeRequestFilterTransactions {
@@ -41,17 +40,59 @@ pub async fn main() -> CarbonResult<()> {
 
     transaction_filters.insert("pumpfun_transaction_filter".to_string(), transaction_filter);
 
-    let yellowstone_grpc = YellowstoneGrpcGeyserClient::new(
-        env::var("GEYSER_URL").unwrap_or_default(),
-        env::var("X_TOKEN").ok(),
+    YellowstoneGrpcGeyserClient::new(
+        grpc_url,
+        Some(x_token),
         Some(CommitmentLevel::Confirmed),
         account_filters,
         transaction_filters,
         Arc::new(RwLock::new(HashSet::new())),
-    );
+    )
+}
+
+pub fn create_helius_websocket_datasource(api_key: String) -> HeliusWebsocket {
+    HeliusWebsocket::new(
+        api_key,
+        carbon_helius_atlas_ws_datasource::Filters {
+            accounts: vec![],
+            transactions: Some(helius::types::enhanced_websocket::RpcTransactionsConfig {
+                filter: helius::types::enhanced_websocket::TransactionSubscribeFilter {
+                    account_include: Some(vec![PUMPFUN_PROGRAM_ID.to_string().clone()]),
+                    account_exclude: None,
+                    account_required: None,
+                    vote: None,
+                    failed: None,
+                    signature: None,
+                },
+                options: helius::types::enhanced_websocket::TransactionSubscribeOptions {
+                    commitment: Some(helius::types::TransactionCommitment::Confirmed),
+                    encoding: None,
+                    transaction_details: Some(helius::types::TransactionDetails::Full),
+                    show_rewards: None,
+                    max_supported_transaction_version: None,
+                },
+            }),
+        },
+        Arc::new(RwLock::new(HashSet::new())),
+    )
+}
+
+#[tokio::main]
+pub async fn main() -> CarbonResult<()> {
+    env_logger::init();
+    dotenv::dotenv().ok();
+
+    // let yellowstone_grpc = create_yellowstone_grpc_datasource(
+    //     env::var("GEYSER_URL").unwrap_or_default(),
+    //     env::var("X_TOKEN").unwrap_or_default(),
+    // );
+
+    let helius_websocket =
+        create_helius_websocket_datasource(env::var("API_KEY").unwrap_or_default());
 
     carbon_core::pipeline::Pipeline::builder()
-        .datasource(yellowstone_grpc)
+        // .datasource(yellowstone_grpc)
+        .datasource(helius_websocket)
         .instruction(PumpfunDecoder, PumpfunInstructionProcessor)
         .build()?
         .run()
@@ -64,11 +105,7 @@ pub struct PumpfunInstructionProcessor;
 
 #[async_trait]
 impl Processor for PumpfunInstructionProcessor {
-    type InputType = (
-        InstructionMetadata,
-        DecodedInstruction<PumpfunInstruction>,
-        Vec<NestedInstruction>,
-    );
+    type InputType = InstructionProcessorInputType<PumpfunInstruction>;
 
     async fn process(
         &mut self,
