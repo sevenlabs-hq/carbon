27482649943eedc1b132005487f4b38af11d3000
Sonic <sonic.from.new.yoke@gmail.com>
2025-02-17 19:15:26 +0200

    fix regression due to incorrect resolve of merge conflicts

--- decoders/raydium-amm-v4-decoder/src/instructions/swap_base_out.rs
+++ decoders/raydium-amm-v4-decoder/src/instructions/swap_base_out.rs
@@ -14,7 +14,7 @@ pub struct SwapBaseOutInstructionAccounts {
     pub amm: solana_sdk::pubkey::Pubkey,
     pub amm_authority: solana_sdk::pubkey::Pubkey,
     pub amm_open_orders: solana_sdk::pubkey::Pubkey,
-    pub amm_target_orders: solana_sdk::pubkey::Pubkey,
+    pub amm_target_orders: Option<solana_sdk::pubkey::Pubkey>,
     pub pool_coin_token_account: solana_sdk::pubkey::Pubkey,
     pub pool_pc_token_account: solana_sdk::pubkey::Pubkey,
     pub serum_program: solana_sdk::pubkey::Pubkey,
@@ -36,31 +36,64 @@ impl carbon_core::deserialize::ArrangeAccounts for SwapBaseOut {
     fn arrange_accounts(
         accounts: &[solana_sdk::instruction::AccountMeta],
     ) -> Option<Self::ArrangedAccounts> {
-        let [token_program, amm, amm_authority, amm_open_orders, amm_target_orders, pool_coin_token_account, pool_pc_token_account, serum_program, serum_market, serum_bids, serum_asks, serum_event_queue, serum_coin_vault_account, serum_pc_vault_account, serum_vault_signer, uer_source_token_account, uer_destination_token_account, user_source_owner] =
-            accounts
-        else {
-            return None;
-        };
+        match accounts.len() {
+            17 => {
+                let [token_program, amm, amm_authority, amm_open_orders, pool_coin_token_account, pool_pc_token_account, serum_program, serum_market, serum_bids, serum_asks, serum_event_queue, serum_coin_vault_account, serum_pc_vault_account, serum_vault_signer, uer_source_token_account, uer_destination_token_account, user_source_owner] =
+                    accounts
+                else {
+                    return None;
+                };
 
-        Some(SwapBaseOutInstructionAccounts {
-            token_program: token_program.pubkey,
-            amm: amm.pubkey,
-            amm_authority: amm_authority.pubkey,
-            amm_open_orders: amm_open_orders.pubkey,
-            amm_target_orders: amm_target_orders.pubkey,
-            pool_coin_token_account: pool_coin_token_account.pubkey,
-            pool_pc_token_account: pool_pc_token_account.pubkey,
-            serum_program: serum_program.pubkey,
-            serum_market: serum_market.pubkey,
-            serum_bids: serum_bids.pubkey,
-            serum_asks: serum_asks.pubkey,
-            serum_event_queue: serum_event_queue.pubkey,
-            serum_coin_vault_account: serum_coin_vault_account.pubkey,
-            serum_pc_vault_account: serum_pc_vault_account.pubkey,
-            serum_vault_signer: serum_vault_signer.pubkey,
-            uer_source_token_account: uer_source_token_account.pubkey,
-            uer_destination_token_account: uer_destination_token_account.pubkey,
-            user_source_owner: user_source_owner.pubkey,
-        })
+                Some(SwapBaseOutInstructionAccounts {
+                    token_program: token_program.pubkey,
+                    amm: amm.pubkey,
+                    amm_authority: amm_authority.pubkey,
+                    amm_open_orders: amm_open_orders.pubkey,
+                    amm_target_orders: None,
+                    pool_coin_token_account: pool_coin_token_account.pubkey,
+                    pool_pc_token_account: pool_pc_token_account.pubkey,
+                    serum_program: serum_program.pubkey,
+                    serum_market: serum_market.pubkey,
+                    serum_bids: serum_bids.pubkey,
+                    serum_asks: serum_asks.pubkey,
+                    serum_event_queue: serum_event_queue.pubkey,
+                    serum_coin_vault_account: serum_coin_vault_account.pubkey,
+                    serum_pc_vault_account: serum_pc_vault_account.pubkey,
+                    serum_vault_signer: serum_vault_signer.pubkey,
+                    uer_source_token_account: uer_source_token_account.pubkey,
+                    uer_destination_token_account: uer_destination_token_account.pubkey,
+                    user_source_owner: user_source_owner.pubkey,
+                })
+            }
+            18 => {
+                let [token_program, amm, amm_authority, amm_open_orders, amm_target_orders, pool_coin_token_account, pool_pc_token_account, serum_program, serum_market, serum_bids, serum_asks, serum_event_queue, serum_coin_vault_account, serum_pc_vault_account, serum_vault_signer, uer_source_token_account, uer_destination_token_account, user_source_owner] =
+                    accounts
+                else {
+                    return None;
+                };
+
+                Some(SwapBaseOutInstructionAccounts {
+                    token_program: token_program.pubkey,
+                    amm: amm.pubkey,
+                    amm_authority: amm_authority.pubkey,
+                    amm_open_orders: amm_open_orders.pubkey,
+                    amm_target_orders: Some(amm_target_orders.pubkey),
+                    pool_coin_token_account: pool_coin_token_account.pubkey,
+                    pool_pc_token_account: pool_pc_token_account.pubkey,
+                    serum_program: serum_program.pubkey,
+                    serum_market: serum_market.pubkey,
+                    serum_bids: serum_bids.pubkey,
+                    serum_asks: serum_asks.pubkey,
+                    serum_event_queue: serum_event_queue.pubkey,
+                    serum_coin_vault_account: serum_coin_vault_account.pubkey,
+                    serum_pc_vault_account: serum_pc_vault_account.pubkey,
+                    serum_vault_signer: serum_vault_signer.pubkey,
+                    uer_source_token_account: uer_source_token_account.pubkey,
+                    uer_destination_token_account: uer_destination_token_account.pubkey,
+                    user_source_owner: user_source_owner.pubkey,
+                })
+            }
+            _ => None,
+        }
     }
 }
