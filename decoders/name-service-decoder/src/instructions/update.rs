7fa2d6cb3ad45258ab4f919bcea9ee4c30b60fc6
ilyxabatko <ilyxabatko@inbox.ru>
2025-02-19 19:40:10 +0400

    feat: name service program decoder

---
+++ decoders/name-service-decoder/src/instructions/update.rs
@@ -0,0 +1,34 @@
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+#[carbon(discriminator = "0x01")]
+pub struct Update {
+    pub offset: u32,
+    pub data: Vec<u8>,
+}
+
+pub struct UpdateInstructionAccounts {
+    pub name_record: solana_sdk::pubkey::Pubkey,
+    pub owner: solana_sdk::pubkey::Pubkey,
+    pub parent_name_record: solana_sdk::pubkey::Pubkey,
+}
+
+impl carbon_core::deserialize::ArrangeAccounts for Update {
+    type ArrangedAccounts = UpdateInstructionAccounts;
+
+    fn arrange_accounts(
+        accounts: &[solana_sdk::instruction::AccountMeta],
+    ) -> Option<Self::ArrangedAccounts> {
+        let [name_record, owner, parent_name_record] = accounts else {
+            return None;
+        };
+
+        Some(UpdateInstructionAccounts {
+            name_record: name_record.pubkey,
+            owner: owner.pubkey,
+            parent_name_record: parent_name_record.pubkey,
+        })
+    }
+}
