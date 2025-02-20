f74dcbccbfc823794a080a6fff038987f0c431e9
Sonic <sonic.from.new.yoke@gmail.com>
2025-02-20 17:36:14 +0200

    fix: restore usage of datasize instead of generated decoder from CLI

--- decoders/raydium-amm-v4-decoder/src/accounts/fees.rs
+++ decoders/raydium-amm-v4-decoder/src/accounts/fees.rs
@@ -5,7 +5,6 @@ pub(crate) const FEES_SIZE: usize = std::mem::size_of::<Fees>();
 #[derive(
     CarbonDeserialize, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq, Clone, Hash,
 )]
-#[carbon(discriminator = "0x979d32738248b324")]
 pub struct Fees {
     pub min_separate_numerator: u64,
     pub min_separate_denominator: u64,
