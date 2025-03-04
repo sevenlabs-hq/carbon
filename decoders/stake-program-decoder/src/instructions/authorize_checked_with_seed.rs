10563d63ceec8c4240b51863ecbd2cf73364691d
Sonic <sonic.from.new.yoke@gmail.com>
2025-03-04 10:56:06 +0200

    feat: add stake program

---
+++ decoders/stake-program-decoder/src/instructions/authorize_checked_with_seed.rs
@@ -0,0 +1,39 @@
+use super::super::types::*;
+
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+#[carbon(discriminator = "0x0ee69aa5e1d1c2d2")]
+pub struct AuthorizeCheckedWithSeed {
+    pub stake_authorize: StakeAuthorize,
+    pub authority_seed: String,
+    pub authority_owner: solana_sdk::pubkey::Pubkey,
+}
+
+pub struct AuthorizeCheckedWithSeedInstructionAccounts {
+    pub stake: solana_sdk::pubkey::Pubkey,
+    pub authority_base: solana_sdk::pubkey::Pubkey,
+    pub clock: solana_sdk::pubkey::Pubkey,
+    pub new_authority: solana_sdk::pubkey::Pubkey,
+}
+
+impl carbon_core::deserialize::ArrangeAccounts for AuthorizeCheckedWithSeed {
+    type ArrangedAccounts = AuthorizeCheckedWithSeedInstructionAccounts;
+
+    fn arrange_accounts(
+        accounts: &[solana_sdk::instruction::AccountMeta],
+    ) -> Option<Self::ArrangedAccounts> {
+        let [stake, authority_base, clock, new_authority, _remaining @ ..] = accounts else {
+            return None;
+        };
+
+        Some(AuthorizeCheckedWithSeedInstructionAccounts {
+            stake: stake.pubkey,
+            authority_base: authority_base.pubkey,
+            clock: clock.pubkey,
+            new_authority: new_authority.pubkey,
+        })
+    }
+}
---
+++ decoders/stake-program-decoder/src/instructions/withdraw.rs
@@ -0,0 +1,37 @@
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+#[carbon(discriminator = "0xb712469c946da122")]
+pub struct Withdraw {
+    pub lamports: u64,
+}
+
+pub struct WithdrawInstructionAccounts {
+    pub from: solana_sdk::pubkey::Pubkey,
+    pub to: solana_sdk::pubkey::Pubkey,
+    pub clock: solana_sdk::pubkey::Pubkey,
+    pub stake_history: solana_sdk::pubkey::Pubkey,
+    pub withdraw_authority: solana_sdk::pubkey::Pubkey,
+}
+
+impl carbon_core::deserialize::ArrangeAccounts for Withdraw {
+    type ArrangedAccounts = WithdrawInstructionAccounts;
+
+    fn arrange_accounts(
+        accounts: &[solana_sdk::instruction::AccountMeta],
+    ) -> Option<Self::ArrangedAccounts> {
+        let [from, to, clock, stake_history, withdraw_authority, _remaining @ ..] = accounts else {
+            return None;
+        };
+
+        Some(WithdrawInstructionAccounts {
+            from: from.pubkey,
+            to: to.pubkey,
+            clock: clock.pubkey,
+            stake_history: stake_history.pubkey,
+            withdraw_authority: withdraw_authority.pubkey,
+        })
+    }
+}
