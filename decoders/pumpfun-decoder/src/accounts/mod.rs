c82d0870b098c2a25437ae0f1aaec8b9c0863692
Sonic <sonic.from.new.yoke@gmail.com>
2025-03-09 22:19:29 +0200

    test: global & last withdraw accounts

--- decoders/pumpfun-decoder/src/accounts/mod.rs
+++ decoders/pumpfun-decoder/src/accounts/mod.rs
@@ -8,6 +8,7 @@ pub mod bonding_curve;
 pub mod global;
 pub mod last_withdraw;
 
+#[allow(clippy::large_enum_variant)]
 pub enum PumpAccount {
     BondingCurve(bonding_curve::BondingCurve),
     Global(global::Global),
@@ -64,6 +65,8 @@ impl<'a> AccountDecoder<'a> for PumpfunDecoder {
 
 #[cfg(test)]
 mod tests {
+    use solana_sdk::pubkey;
+
     use super::*;
 
     #[test]
@@ -110,6 +113,89 @@ mod tests {
                 assert_eq!(expected_bonding_curve.complete, bonding_curve.complete);
             }
             _ => panic!("Expected BondingCurve"),
+        }
+    }
+
+    #[test]
+    fn test_decode_global_account() {
+        // Arrange
+        let expected_global_account = global::Global {
+            initialized: true,
+            authority: pubkey!("DCpJReAfonSrgohiQbTmKKbjbqVofspFRHz9yQikzooP"),
+            fee_recipient: pubkey!("62qc2CNXwrYqQScmEdiZFFAnJR262PxWEuNQtxfafNgV"),
+            initial_virtual_token_reserves: 1073000000000000,
+            initial_virtual_sol_reserves: 30000000000,
+            initial_real_token_reserves: 793100000000000,
+            token_total_supply: 1000000000000000,
+            fee_basis_points: 100,
+            ..Default::default()
+        };
+
+        // Act
+        let decoder = PumpfunDecoder;
+        let account = carbon_test_utils::read_account("tests/fixtures/global_account.json")
+            .expect("read fixture");
+        let decoded_account = decoder.decode_account(&account).expect("decode fixture");
+
+        // Assert
+        match decoded_account.data {
+            PumpAccount::Global(global_account) => {
+                assert_eq!(
+                    expected_global_account.initialized,
+                    global_account.initialized
+                );
+                assert_eq!(expected_global_account.authority, global_account.authority);
+                assert_eq!(
+                    expected_global_account.fee_recipient,
+                    global_account.fee_recipient
+                );
+                assert_eq!(
+                    expected_global_account.initial_virtual_token_reserves,
+                    global_account.initial_virtual_token_reserves
+                );
+                assert_eq!(
+                    expected_global_account.initial_virtual_sol_reserves,
+                    global_account.initial_virtual_sol_reserves
+                );
+                assert_eq!(
+                    expected_global_account.initial_real_token_reserves,
+                    global_account.initial_real_token_reserves
+                );
+                assert_eq!(
+                    expected_global_account.token_total_supply,
+                    global_account.token_total_supply
+                );
+                assert_eq!(
+                    expected_global_account.fee_basis_points,
+                    global_account.fee_basis_points
+                );
+            }
+            _ => panic!("Expected Global"),
+        }
+    }
+
+    #[test]
+    fn test_decode_last_withdraw_account() {
+        // Arrange
+        let expected_last_withdraw_account = last_withdraw::LastWithdraw {
+            last_withdraw_timestamp: 1741550682,
+        };
+
+        // Act
+        let decoder = PumpfunDecoder;
+        let account = carbon_test_utils::read_account("tests/fixtures/last_withdraw_account.json")
+            .expect("read fixture");
+        let decoded_account = decoder.decode_account(&account).expect("decode fixture");
+
+        // Assert
+        match decoded_account.data {
+            PumpAccount::LastWithdraw(last_withdraw) => {
+                assert_eq!(
+                    expected_last_withdraw_account.last_withdraw_timestamp,
+                    last_withdraw.last_withdraw_timestamp
+                );
+            }
+            _ => panic!("Expected LastWithdraw"),
         }
     }
 }
