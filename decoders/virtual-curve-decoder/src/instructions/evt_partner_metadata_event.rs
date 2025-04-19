de42888348a61174e23468c8620c9db4bf0daaf6
Sonic <sonic.from.new.yoke@gmail.com>
2025-04-19 20:40:46 +0300

    feat: add meteora virtual curve integration

---
+++ decoders/virtual-curve-decoder/src/instructions/evt_partner_metadata_event.rs
@@ -0,0 +1,7 @@
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+#[carbon(discriminator = "0xe445a52e51cb9a1dc87f06370d200896")]
+pub struct EvtPartnerMetadataEvent {}
---
+++ decoders/virtual-curve-decoder/src/types/create_virtual_pool_metadata_parameters.rs
@@ -0,0 +1,13 @@
+use alloc::string::String;
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+pub struct CreateVirtualPoolMetadataParameters {
+    #[serde(with = "serde_big_array::BigArray")]
+    pub padding: [u8; 96],
+    pub name: String,
+    pub website: String,
+    pub logo: String,
+}
