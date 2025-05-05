bb4ec005f2af0abda43cdc39d9a1c0539f0f3ba3
Sonic <sonic.from.new.yoke@gmail.com>
2025-05-05 23:27:48 +0300

    feat: init feature

---
+++ crates/gql-server/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod mutation;
+pub mod query;
+pub mod subscription;
+pub mod types;
---
+++ crates/gql-server/src/mutation.rs
@@ -0,0 +1,10 @@
+use juniper_codegen::graphql_object;
+
+pub struct Immutable;
+
+#[graphql_object]
+impl Immutable {
+    fn api_version() -> &'static str {
+        "1.0"
+    }
+}
