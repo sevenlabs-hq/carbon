9b048dc2911e43342dc069780e39b076e8c39e71
Mesteery <mestery@protonmail.com>
2025-05-11 15:53:22 +0200

    fix: Raydium CLMM events discriminators (#294)

--- decoders/raydium-clmm-decoder/src/instructions/increase_liquidity_event.rs
+++ decoders/raydium-clmm-decoder/src/instructions/increase_liquidity_event.rs
@@ -3,7 +3,7 @@ use carbon_core::{borsh, CarbonDeserialize};
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
-#[carbon(discriminator = "0xe445a52e51cb9a1d314f69d420221e54")]
+#[carbon(discriminator = "0x314f69d420221e54")]
 pub struct IncreaseLiquidityEvent {
     pub position_nft_mint: solana_pubkey::Pubkey,
     pub liquidity: u128,
