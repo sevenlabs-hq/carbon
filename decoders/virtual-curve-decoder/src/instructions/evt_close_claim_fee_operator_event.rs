1ff4529bd3d6b671bd3e654919dc3bf451f641bf
Mesteery <mestery@protonmail.com>
2025-05-29 22:27:49 +0200

    fix: meteora virtual curve events

--- decoders/virtual-curve-decoder/src/instructions/evt_close_claim_fee_operator_event.rs
+++ decoders/virtual-curve-decoder/src/instructions/evt_close_claim_fee_operator_event.rs
@@ -4,4 +4,7 @@ use carbon_core::{borsh, CarbonDeserialize};
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
 #[carbon(discriminator = "0xe445a52e51cb9a1d6f2725376ed8c217")]
-pub struct EvtCloseClaimFeeOperatorEvent {}
+pub struct EvtCloseClaimFeeOperatorEvent {
+    pub claim_fee_operator: solana_pubkey::Pubkey,
+    pub operator: solana_pubkey::Pubkey,
+}
--- decoders/virtual-curve-decoder/src/instructions/evt_initialize_pool_event.rs
+++ decoders/virtual-curve-decoder/src/instructions/evt_initialize_pool_event.rs
@@ -4,4 +4,11 @@ use carbon_core::{borsh, CarbonDeserialize};
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
 #[carbon(discriminator = "0xe445a52e51cb9a1de432f655cb428625")]
-pub struct EvtInitializePoolEvent {}
+pub struct EvtInitializePoolEvent {
+    pub pool: solana_pubkey::Pubkey,
+    pub config: solana_pubkey::Pubkey,
+    pub creator: solana_pubkey::Pubkey,
+    pub base_mint: solana_pubkey::Pubkey,
+    pub pool_type: u8,
+    pub activation_point: u64,
+}
