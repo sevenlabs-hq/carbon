00c4206e8e6339b3cf837b3e658fd19719f45184
Mesteery <mestery@protonmail.com>
2025-05-29 22:41:17 +0200

    fix: meteora virtual curve events (#328)

--- decoders/virtual-curve-decoder/src/instructions/evt_virtual_pool_metadata_event.rs
+++ decoders/virtual-curve-decoder/src/instructions/evt_virtual_pool_metadata_event.rs
@@ -4,4 +4,7 @@ use carbon_core::{borsh, CarbonDeserialize};
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
 #[carbon(discriminator = "0xe445a52e51cb9a1dbc12484cc35b264a")]
-pub struct EvtVirtualPoolMetadataEvent {}
+pub struct EvtVirtualPoolMetadataEvent {
+    pub virtual_pool_metadata: solana_pubkey::Pubkey,
+    pub virtual_pool: solana_pubkey::Pubkey,
+}
