5d5499a4d79fbd8301febeedcf0933ff9f25fb4e
kratos2377 <shobityadav23@gmail.com>
2025-07-20 19:29:32 +0530

    Adding Scalable UI amount and Pausable Extension instruction for Token2022

---
+++ decoders/token-2022-decoder/src/instructions/initialize_mint_pausable.rs
@@ -0,0 +1,25 @@
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Clone)]
+#[carbon(discriminator = "0x17")]
+pub struct InitializeMintPausable {
+    pub authority: solana_pubkey::Pubkey
+}
+
+pub struct InitializeMintPausableInstructionAccounts {
+    pub mint_account: solana_pubkey::Pubkey,
+}
+
+impl carbon_core::deserialize::ArrangeAccounts for InitializeMintPausable {
+    type ArrangedAccounts = InitializeMintPausableInstructionAccounts;
+
+    fn arrange_accounts(
+        accounts: &[solana_instruction::AccountMeta],
+    ) -> Option<Self::ArrangedAccounts> {
+        let [mint, _remaining @ ..] = accounts else {
+            return None;
+        };
+
+        Some(InitializeMintPausableInstructionAccounts { mint_account: mint.pubkey })
+    }
+}
