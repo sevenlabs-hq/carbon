d6bb81b8d4f29bdccc01950ece76aefb87a787aa
kralonur <kralonur1998@gmail.com>
2025-07-31 14:57:03 +0300

    refactor: update pumpswap decoder

--- decoders/pump-swap-decoder/src/instructions/mod.rs
+++ decoders/pump-swap-decoder/src/instructions/mod.rs
@@ -1,8 +1,14 @@
 use crate::PROGRAM_ID;
 
 use super::PumpSwapDecoder;
+pub mod admin_set_coin_creator;
+pub mod admin_set_coin_creator_event;
+pub mod admin_update_token_incentives;
+pub mod admin_update_token_incentives_event;
 pub mod buy;
 pub mod buy_event;
+pub mod claim_token_incentives;
+pub mod claim_token_incentives_event;
 pub mod collect_coin_creator_fee;
 pub mod collect_coin_creator_fee_event;
 pub mod create_config;
@@ -20,6 +26,8 @@ pub mod sell_event;
 pub mod set_bonding_curve_coin_creator_event;
 pub mod set_coin_creator;
 pub mod set_metaplex_coin_creator_event;
+pub mod sync_user_volume_accumulator;
+pub mod sync_user_volume_accumulator_event;
 pub mod update_admin;
 pub mod update_admin_event;
 pub mod update_fee_config;
@@ -38,7 +46,10 @@ pub mod withdraw_event;
     Hash,
 )]
 pub enum PumpSwapInstruction {
+    AdminSetCoinCreator(admin_set_coin_creator::AdminSetCoinCreator),
+    AdminUpdateTokenIncentives(admin_update_token_incentives::AdminUpdateTokenIncentives),
     Buy(buy::Buy),
+    ClaimTokenIncentives(claim_token_incentives::ClaimTokenIncentives),
     CollectCoinCreatorFee(collect_coin_creator_fee::CollectCoinCreatorFee),
     CreateConfig(create_config::CreateConfig),
     CreatePool(create_pool::CreatePool),
@@ -47,10 +58,16 @@ pub enum PumpSwapInstruction {
     ExtendAccount(extend_account::ExtendAccount),
     Sell(sell::Sell),
     SetCoinCreator(set_coin_creator::SetCoinCreator),
+    SyncUserVolumeAccumulator(sync_user_volume_accumulator::SyncUserVolumeAccumulator),
     UpdateAdmin(update_admin::UpdateAdmin),
     UpdateFeeConfig(update_fee_config::UpdateFeeConfig),
     Withdraw(withdraw::Withdraw),
+    AdminSetCoinCreatorEvent(admin_set_coin_creator_event::AdminSetCoinCreatorEvent),
+    AdminUpdateTokenIncentivesEvent(
+        admin_update_token_incentives_event::AdminUpdateTokenIncentivesEvent,
+    ),
     BuyEvent(buy_event::BuyEvent),
+    ClaimTokenIncentivesEvent(claim_token_incentives_event::ClaimTokenIncentivesEvent),
     CollectCoinCreatorFeeEvent(collect_coin_creator_fee_event::CollectCoinCreatorFeeEvent),
     CreateConfigEvent(create_config_event::CreateConfigEvent),
     CreatePoolEvent(create_pool_event::CreatePoolEvent),
@@ -62,6 +79,9 @@ pub enum PumpSwapInstruction {
         set_bonding_curve_coin_creator_event::SetBondingCurveCoinCreatorEvent,
     ),
     SetMetaplexCoinCreatorEvent(set_metaplex_coin_creator_event::SetMetaplexCoinCreatorEvent),
+    SyncUserVolumeAccumulatorEvent(
+        sync_user_volume_accumulator_event::SyncUserVolumeAccumulatorEvent,
+    ),
     UpdateAdminEvent(update_admin_event::UpdateAdminEvent),
     UpdateFeeConfigEvent(update_fee_config_event::UpdateFeeConfigEvent),
     WithdrawEvent(withdraw_event::WithdrawEvent),
@@ -78,7 +98,10 @@ impl carbon_core::instruction::InstructionDecoder<'_> for PumpSwapDecoder {
             return None;
         }
         carbon_core::try_decode_instructions!(instruction,
+            PumpSwapInstruction::AdminSetCoinCreator => admin_set_coin_creator::AdminSetCoinCreator,
+            PumpSwapInstruction::AdminUpdateTokenIncentives => admin_update_token_incentives::AdminUpdateTokenIncentives,
             PumpSwapInstruction::Buy => buy::Buy,
+            PumpSwapInstruction::ClaimTokenIncentives => claim_token_incentives::ClaimTokenIncentives,
             PumpSwapInstruction::CollectCoinCreatorFee => collect_coin_creator_fee::CollectCoinCreatorFee,
             PumpSwapInstruction::CreateConfig => create_config::CreateConfig,
             PumpSwapInstruction::CreatePool => create_pool::CreatePool,
@@ -87,10 +110,14 @@ impl carbon_core::instruction::InstructionDecoder<'_> for PumpSwapDecoder {
             PumpSwapInstruction::ExtendAccount => extend_account::ExtendAccount,
             PumpSwapInstruction::Sell => sell::Sell,
             PumpSwapInstruction::SetCoinCreator => set_coin_creator::SetCoinCreator,
+            PumpSwapInstruction::SyncUserVolumeAccumulator => sync_user_volume_accumulator::SyncUserVolumeAccumulator,
             PumpSwapInstruction::UpdateAdmin => update_admin::UpdateAdmin,
             PumpSwapInstruction::UpdateFeeConfig => update_fee_config::UpdateFeeConfig,
             PumpSwapInstruction::Withdraw => withdraw::Withdraw,
+            PumpSwapInstruction::AdminSetCoinCreatorEvent => admin_set_coin_creator_event::AdminSetCoinCreatorEvent,
+            PumpSwapInstruction::AdminUpdateTokenIncentivesEvent => admin_update_token_incentives_event::AdminUpdateTokenIncentivesEvent,
             PumpSwapInstruction::BuyEvent => buy_event::BuyEvent,
+            PumpSwapInstruction::ClaimTokenIncentivesEvent => claim_token_incentives_event::ClaimTokenIncentivesEvent,
             PumpSwapInstruction::CollectCoinCreatorFeeEvent => collect_coin_creator_fee_event::CollectCoinCreatorFeeEvent,
             PumpSwapInstruction::CreateConfigEvent => create_config_event::CreateConfigEvent,
             PumpSwapInstruction::CreatePoolEvent => create_pool_event::CreatePoolEvent,
@@ -100,6 +127,7 @@ impl carbon_core::instruction::InstructionDecoder<'_> for PumpSwapDecoder {
             PumpSwapInstruction::SellEvent => sell_event::SellEvent,
             PumpSwapInstruction::SetBondingCurveCoinCreatorEvent => set_bonding_curve_coin_creator_event::SetBondingCurveCoinCreatorEvent,
             PumpSwapInstruction::SetMetaplexCoinCreatorEvent => set_metaplex_coin_creator_event::SetMetaplexCoinCreatorEvent,
+            PumpSwapInstruction::SyncUserVolumeAccumulatorEvent => sync_user_volume_accumulator_event::SyncUserVolumeAccumulatorEvent,
             PumpSwapInstruction::UpdateAdminEvent => update_admin_event::UpdateAdminEvent,
             PumpSwapInstruction::UpdateFeeConfigEvent => update_fee_config_event::UpdateFeeConfigEvent,
             PumpSwapInstruction::WithdrawEvent => withdraw_event::WithdrawEvent,
