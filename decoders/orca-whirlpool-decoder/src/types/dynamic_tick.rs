6e12e5e4339b1e04b8d0795583487c61cc31d64b
Mesteery <mestery@protonmail.com>
2025-08-21 18:23:03 +0200

    support orca dynamic tick array

---
+++ decoders/orca-whirlpool-decoder/src/types/dynamic_tick.rs
@@ -0,0 +1,20 @@
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash, Copy,
+)]
+pub enum DynamicTick {
+    Uninitialized,
+    Initialized(DynamicTickData),
+}
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash, Copy,
+)]
+pub struct DynamicTickData {
+    pub liquidity_net: i128,
+    pub liquidity_gross: u128,
+    pub fee_growth_outside_a: u128,
+    pub fee_growth_outside_b: u128,
+    pub reward_growths_outside: [u128; 3],
+}
