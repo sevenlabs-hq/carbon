cdf51bba6fb39b18b8281248820a73f170bb0345
Mesteery <mestery@protonmail.com>
2025-08-30 07:41:25 +0200

    fix serde implementation for whirlpool DynamicTick
    enum variant discriminator should be u8 instead of u32
    borsh deserialize it already correctly

--- decoders/orca-whirlpool-decoder/src/accounts/dynamic_tick_array.rs
+++ decoders/orca-whirlpool-decoder/src/accounts/dynamic_tick_array.rs
@@ -13,3 +13,35 @@ pub struct DynamicTickArray {
     #[serde(with = "serde_big_array::BigArray")]
     pub ticks: [DynamicTick; 88],
 }
+
+#[cfg(test)]
+mod tests {
+    use carbon_core::account::AccountDecoder;
+
+    use crate::{accounts::WhirlpoolAccount, types::DynamicTick, OrcaWhirlpoolDecoder};
+
+    #[test]
+    fn test_decode_dynamic_tick_array_accounts() {
+        let decoder = OrcaWhirlpoolDecoder;
+        let account =
+            carbon_test_utils::read_account("tests/fixtures/dynamic_tick_array_account0.json")
+                .expect("read fixture");
+
+        let decoded_account = decoder.decode_account(&account).expect("decode fixture");
+
+        match decoded_account.data {
+            WhirlpoolAccount::DynamicTickArray(account) => {
+                println!("{:#?}", account);
+                for i in 0..account.ticks.len() {
+                    let tick = account.ticks[i];
+                    let is_uninitialized = account.tick_bitmap & (1 << i) == 0;
+
+                    if is_uninitialized {
+                        assert_eq!(tick, DynamicTick::Uninitialized);
+                    }
+                }
+            }
+            _ => panic!("Expected DynamicTickArray account"),
+        }
+    }
+}
