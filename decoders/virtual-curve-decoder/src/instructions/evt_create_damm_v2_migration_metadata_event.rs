7f7b93fc967b3945e69c9ec4c57f733f4d923806
unbound <draskovic26z@gmail.com>
2025-09-05 10:36:33 +0200

    fixes duplicate meteora dbc program

--- decoders/virtual-curve-decoder/src/instructions/evt_create_damm_v2_migration_metadata_event.rs
+++
@@ -1,9 +0,0 @@
-use carbon_core::{borsh, CarbonDeserialize};
-
-#[derive(
-    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
-)]
-#[carbon(discriminator = "0xe445a52e51cb9a1d676f84a88cfd9672")]
-pub struct EvtCreateDammV2MigrationMetadataEvent {
-    pub virtual_pool: solana_pubkey::Pubkey,
-}
