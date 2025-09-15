424d7cc285e893beda57ce6198564e0a195afa80
Nikita Kuznetsov <n@8gen.team>
2025-09-15 17:51:36 +0200

    feat: update pumpswap to 09.2025 update

--- decoders/pump-swap-decoder/src/instructions/admin_update_token_incentives_event.rs
+++ decoders/pump-swap-decoder/src/instructions/admin_update_token_incentives_event.rs
@@ -9,4 +9,7 @@ pub struct AdminUpdateTokenIncentivesEvent {
     pub end_time: i64,
     pub day_number: u64,
     pub token_supply_per_day: u64,
+    pub mint: solana_pubkey::Pubkey,
+    pub seconds_in_a_day: i64,
+    pub timestamp: i64,
 }
--- decoders/pump-swap-decoder/src/types/init_user_volume_accumulator_event.rs
+++ decoders/pump-swap-decoder/src/types/init_user_volume_accumulator_event.rs
@@ -3,10 +3,8 @@ use carbon_core::{borsh, CarbonDeserialize};
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
-#[carbon(discriminator = "0xe445a52e51cb9a1dc57aa77c74515bff")]
-pub struct SyncUserVolumeAccumulatorEvent {
+pub struct InitUserVolumeAccumulatorEvent {
+    pub payer: solana_pubkey::Pubkey,
     pub user: solana_pubkey::Pubkey,
-    pub total_claimed_tokens_before: u64,
-    pub total_claimed_tokens_after: u64,
     pub timestamp: i64,
 }
