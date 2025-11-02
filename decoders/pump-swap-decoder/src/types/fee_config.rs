15db3ba3e0cfd6b52326beae82c7a13e5bc46d8d
Oleksandr Ovcharov <oleksandr.sheff3rd@gmail.com>
2025-11-02 19:53:55 +0100

    feat: upgrade Solana version (#435)
    Co-authored-by: technoch1ef <oleskandr.sheff3rd@gmail.com>

--- decoders/pump-swap-decoder/src/types/fee_config.rs
+++ decoders/pump-swap-decoder/src/types/fee_config.rs
@@ -1,9 +1,6 @@
 use super::*;
 
-use carbon_core::{
-    borsh::{self, maybestd::vec::Vec},
-    CarbonDeserialize,
-};
+use carbon_core::{borsh, CarbonDeserialize};
 
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
