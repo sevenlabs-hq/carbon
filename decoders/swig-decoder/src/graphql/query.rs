27f25d3b6c07639b949188c42d813a8bff9001e7
azoth zephyr <contact@apolloni.us>
2025-11-06 14:45:21 -0800

    add swig decoder

---
+++ decoders/swig-decoder/src/graphql/query.rs
@@ -0,0 +1,13 @@
+use juniper::{graphql_object, FieldResult};
+use std::str::FromStr;
+
+pub struct QueryRoot;
+
+#[graphql_object(context = crate::graphql::context::GraphQLContext)]
+impl QueryRoot {
+    // Accounts
+    
+    // Instructions (per-instruction list and lookup by signature+index)
+                                                                                                            
+    }
+
---
+++ decoders/swig-decoder/src/instructions/graphql/mod.rs
@@ -0,0 +1,25 @@
+
+
+use juniper::GraphQLObject;
+
+#[derive(Debug, Clone, GraphQLObject)]
+#[graphql(name = "InstructionMetadata")]
+pub struct InstructionMetadataGraphQL {
+    pub signature: String,
+    pub instruction_index: carbon_core::graphql::primitives::U32,
+    pub stack_height: carbon_core::graphql::primitives::U32,
+    pub slot: Option<carbon_core::graphql::primitives::U64>,
+}
+
+impl From<carbon_core::postgres::metadata::InstructionRowMetadata> for InstructionMetadataGraphQL {
+    fn from(metadata: carbon_core::postgres::metadata::InstructionRowMetadata) -> Self {
+        Self {
+            signature: metadata.signature,
+            instruction_index: carbon_core::graphql::primitives::U32((*metadata.instruction_index) as u32),
+            stack_height: carbon_core::graphql::primitives::U32((*metadata.stack_height) as u32),
+            slot: metadata
+                .slot
+                .map(|v| carbon_core::graphql::primitives::U64(*v)),
+        }
+    }
+}
