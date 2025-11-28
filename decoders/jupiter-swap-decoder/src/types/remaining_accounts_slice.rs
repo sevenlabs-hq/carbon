5a38ab49ff6387ddcee398a2a1985c93f6122725
arrayappy <arrayappy@gmail.com>
2025-11-28 09:34:23 +0530

    cleanup: remove old jupiter swap decoder

--- decoders/jupiter-swap-decoder/src/types/remaining_accounts_slice.rs
+++
@@ -1,9 +0,0 @@
-use carbon_core::{borsh, CarbonDeserialize};
-
-#[derive(
-    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
-)]
-pub struct RemainingAccountsSlice {
-    pub accounts_type: u8,
-    pub length: u8,
-}
