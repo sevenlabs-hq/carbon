11a00d7288feeec98fcc21df00acabc09165cff1
arrayappy <arrayappy@gmail.com>
2025-11-28 09:34:23 +0530

    cleanup: remove old jupiter swap decoder

--- decoders/jupiter-swap-decoder/src/types/token_ledger.rs
+++
@@ -1,9 +0,0 @@
-use carbon_core::{borsh, CarbonDeserialize};
-
-#[derive(
-    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
-)]
-pub struct TokenLedger {
-    pub token_account: solana_pubkey::Pubkey,
-    pub amount: u64,
-}
