44b0cbba0c33c7d110e72047a2ba0ccc748b0376
arrayappy <arrayappy@gmail.com>
2025-12-23 15:51:35 +0530

    chore: match versions crates with main Cargo.toml

--- packages/versions/src/index.ts
+++ packages/versions/src/index.ts
@@ -58,12 +58,10 @@ export const VERSIONS: Record<string, CrateDependency> = {
     'spl-token-group-interface': '0.7.1',
     'spl-type-length-value': '0.9.0',
     'yellowstone-grpc-client': {
-        git: 'https://github.com/rpcpool/yellowstone-grpc',
-        rev: '73c43e1112f6b3432a6b2df9bad73438f6c51034',
+        version: '10.0.0',
     },
     'yellowstone-grpc-proto': {
-        git: 'https://github.com/rpcpool/yellowstone-grpc',
-        rev: '73c43e1112f6b3432a6b2df9bad73438f6c51034',
+        version: '10.0.0',
         features: ['convert'],
     },
     /// Other crates
