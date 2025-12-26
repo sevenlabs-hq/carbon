f880f284d8862866852422f21ea210a3dc78f24e
Dmitry <binqbit@gmail.com>
2025-12-26 13:55:00 +0200

    fix: add #[serde(transparent)] attribute to OptionBool struct

--- decoders/pumpfun-decoder/src/types/option_bool.rs
+++ decoders/pumpfun-decoder/src/types/option_bool.rs
@@ -3,4 +3,5 @@ use carbon_core::{borsh, CarbonDeserialize};
 #[derive(
     CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
 )]
+#[serde(transparent)]
 pub struct OptionBool(pub bool);
