91104947ec6e2c549cbf3bc1dc250c3594435e3c
arrayappy <arrayappy@gmail.com>
2026-01-08 12:57:26 +0530

    cleanup: remove old damm v2 decoder

--- decoders/meteora-damm-v2-decoder/src/types/dynamic_config_parameters.rs
+++
@@ -1,8 +0,0 @@
-use carbon_core::{borsh, CarbonDeserialize};
-
-#[derive(
-    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
-)]
-pub struct DynamicConfigParameters {
-    pub pool_creator_authority: solana_pubkey::Pubkey,
-}
