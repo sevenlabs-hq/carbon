ca9f556ecdf9ae2fc665f4708888b26e51333774
arrayappy <arrayappy@gmail.com>
2026-01-28 21:04:41 +0530

    fix: add unified account type for decoded instruction data and arrange accounts

--- crates/macros/src/try_decode_ixs.rs
+++ crates/macros/src/try_decode_ixs.rs
@@ -1,18 +1,19 @@
 #[macro_export]
 macro_rules! try_decode_instructions {
-    ($instruction:expr, $($variant:path => $ty:ty),* $(,)?) => {{
-        use carbon_core::deserialize::CarbonDeserialize;
+    ($instruction:expr, $program_id:expr, $($enum_name:ident :: $variant:ident => $ty:ty),* $(,)?) => {{
+        use carbon_core::deserialize::ArrangeAccounts;
         $(
-            if let Some(decoded_instruction) = <$ty>::deserialize($instruction.data.as_slice()) {
-                Some(carbon_core::instruction::DecodedInstruction {
-                    program_id: $instruction.program_id,
-                    accounts: $instruction.accounts.clone(),
-                    data: $variant(decoded_instruction),
-                })
-            } else
+            if let Some(data) = <$ty>::decode($instruction.data.as_slice()) {
+                if let Some(accounts) = <$ty>::arrange_accounts(&$instruction.accounts) {
+                    let result = $enum_name::$variant {
+                        program_id: $program_id,
+                        data: data,
+                        accounts: accounts,
+                    };
+                    return Some(result);
+                }
+            }
         )*
-        {
-            None
-        }
+        None
     }};
 }
