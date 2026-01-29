12727578cd39e10155bfd0d78c5071c9ac9dc741
arrayappy <arrayappy@gmail.com>
2026-01-29 14:49:15 +0530

    chore: cargo fmt

--- crates/core/src/collection.rs
+++ crates/core/src/collection.rs
@@ -5,8 +5,6 @@ pub trait InstructionDecoderCollection:
 {
     type InstructionType: Clone + std::fmt::Debug + PartialEq + Eq + Send + Sync + 'static;
 
-    fn parse_instruction(
-        instruction: &solana_instruction::Instruction,
-    ) -> Option<Self>;
+    fn parse_instruction(instruction: &solana_instruction::Instruction) -> Option<Self>;
     fn get_type(&self) -> Self::InstructionType;
 }
