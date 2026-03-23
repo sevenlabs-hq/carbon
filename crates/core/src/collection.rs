e572189263c9c1fd674fac4a59286289cbf3bac3
arrayappy <arrayappy@gmail.com>
2026-03-23 17:52:58 +0530

    feat: add cpi_events support

--- crates/core/src/collection.rs
+++ crates/core/src/collection.rs
@@ -1,3 +1,4 @@
+use crate::instruction::InstructionMetadata;
 use serde::Serialize;
 
 pub trait InstructionDecoderCollection:
@@ -5,6 +6,9 @@ pub trait InstructionDecoderCollection:
 {
     type InstructionType: Clone + std::fmt::Debug + PartialEq + Eq + Send + Sync + 'static;
 
-    fn parse_instruction(instruction: &solana_instruction::Instruction) -> Option<Self>;
+    fn parse_instruction(
+        metadata: &InstructionMetadata,
+        instruction: &solana_instruction::Instruction,
+    ) -> Vec<Self>;
     fn get_type(&self) -> Self::InstructionType;
 }
