a5f16e8a7c283ec2312e68c68341e54583276b0d
arrayappy <arrayappy@gmail.com>
2026-04-11 21:32:44 +0530

    refactor: raydium-clmm and pumpswap decoders with new CpiEvent decoding logic

--- decoders/raydium-clmm-decoder/src/instructions/mod.rs
+++ decoders/raydium-clmm-decoder/src/instructions/mod.rs
@@ -173,11 +173,10 @@ pub enum RaydiumClmmInstruction {
         data: UpdateRewardInfos,
         accounts: UpdateRewardInfosInstructionAccounts,
     },
-    // Anchor CPI Event Instruction
     CpiEvent {
         program_id: solana_pubkey::Pubkey,
         data: CpiEvent,
-        accounts: CpiEventInstructionAccounts,
+        accounts: Option<CpiEventInstructionAccounts>,
     },
 }
 
@@ -186,41 +185,79 @@ impl carbon_core::instruction::InstructionDecoder<'_> for RaydiumClmmDecoder {
 
     fn decode_instruction(
         &self,
+        metadata: &carbon_core::instruction::InstructionMetadata,
         instruction: &solana_instruction::Instruction,
     ) -> Option<Self::InstructionType> {
+        self.decode_instructions(metadata, instruction)
+            .into_iter()
+            .next()
+    }
+
+    fn decode_instructions(
+        &self,
+        metadata: &carbon_core::instruction::InstructionMetadata,
+        instruction: &solana_instruction::Instruction,
+    ) -> Vec<Self::InstructionType> {
+        use carbon_core::deserialize::ArrangeAccounts as _;
         if instruction.program_id != PROGRAM_ID {
-            return None;
+            return Vec::new();
         }
 
-        carbon_core::try_decode_instructions!(
-            instruction,
-            PROGRAM_ID,
-            RaydiumClmmInstruction::ClosePosition => ClosePosition,
-            RaydiumClmmInstruction::CollectFundFee => CollectFundFee,
-            RaydiumClmmInstruction::CollectProtocolFee => CollectProtocolFee,
-            RaydiumClmmInstruction::CollectRemainingRewards => CollectRemainingRewards,
-            RaydiumClmmInstruction::CreateAmmConfig => CreateAmmConfig,
-            RaydiumClmmInstruction::CreateOperationAccount => CreateOperationAccount,
-            RaydiumClmmInstruction::CreatePool => CreatePool,
-            RaydiumClmmInstruction::CreateSupportMintAssociated => CreateSupportMintAssociated,
-            RaydiumClmmInstruction::DecreaseLiquidity => DecreaseLiquidity,
-            RaydiumClmmInstruction::DecreaseLiquidityV2 => DecreaseLiquidityV2,
-            RaydiumClmmInstruction::IncreaseLiquidity => IncreaseLiquidity,
-            RaydiumClmmInstruction::IncreaseLiquidityV2 => IncreaseLiquidityV2,
-            RaydiumClmmInstruction::InitializeReward => InitializeReward,
-            RaydiumClmmInstruction::OpenPosition => OpenPosition,
-            RaydiumClmmInstruction::OpenPositionV2 => OpenPositionV2,
-            RaydiumClmmInstruction::OpenPositionWithToken22Nft => OpenPositionWithToken22Nft,
-            RaydiumClmmInstruction::SetRewardParams => SetRewardParams,
-            RaydiumClmmInstruction::Swap => Swap,
-            RaydiumClmmInstruction::SwapRouterBaseIn => SwapRouterBaseIn,
-            RaydiumClmmInstruction::SwapV2 => SwapV2,
-            RaydiumClmmInstruction::TransferRewardOwner => TransferRewardOwner,
-            RaydiumClmmInstruction::UpdateAmmConfig => UpdateAmmConfig,
-            RaydiumClmmInstruction::UpdateOperationAccount => UpdateOperationAccount,
-            RaydiumClmmInstruction::UpdatePoolStatus => UpdatePoolStatus,
-            RaydiumClmmInstruction::UpdateRewardInfos => UpdateRewardInfos,
-            RaydiumClmmInstruction::CpiEvent => CpiEvent,
-        )
+        let decoded_instruction = (|| {
+            carbon_core::try_decode_instructions!(
+                instruction,
+                PROGRAM_ID,
+                RaydiumClmmInstruction::ClosePosition => ClosePosition,
+                RaydiumClmmInstruction::CollectFundFee => CollectFundFee,
+                RaydiumClmmInstruction::CollectProtocolFee => CollectProtocolFee,
+                RaydiumClmmInstruction::CollectRemainingRewards => CollectRemainingRewards,
+                RaydiumClmmInstruction::CreateAmmConfig => CreateAmmConfig,
+                RaydiumClmmInstruction::CreateOperationAccount => CreateOperationAccount,
+                RaydiumClmmInstruction::CreatePool => CreatePool,
+                RaydiumClmmInstruction::CreateSupportMintAssociated => CreateSupportMintAssociated,
+                RaydiumClmmInstruction::DecreaseLiquidity => DecreaseLiquidity,
+                RaydiumClmmInstruction::DecreaseLiquidityV2 => DecreaseLiquidityV2,
+                RaydiumClmmInstruction::IncreaseLiquidity => IncreaseLiquidity,
+                RaydiumClmmInstruction::IncreaseLiquidityV2 => IncreaseLiquidityV2,
+                RaydiumClmmInstruction::InitializeReward => InitializeReward,
+                RaydiumClmmInstruction::OpenPosition => OpenPosition,
+                RaydiumClmmInstruction::OpenPositionV2 => OpenPositionV2,
+                RaydiumClmmInstruction::OpenPositionWithToken22Nft => OpenPositionWithToken22Nft,
+                RaydiumClmmInstruction::SetRewardParams => SetRewardParams,
+                RaydiumClmmInstruction::Swap => Swap,
+                RaydiumClmmInstruction::SwapRouterBaseIn => SwapRouterBaseIn,
+                RaydiumClmmInstruction::SwapV2 => SwapV2,
+                RaydiumClmmInstruction::TransferRewardOwner => TransferRewardOwner,
+                RaydiumClmmInstruction::UpdateAmmConfig => UpdateAmmConfig,
+                RaydiumClmmInstruction::UpdateOperationAccount => UpdateOperationAccount,
+                RaydiumClmmInstruction::UpdatePoolStatus => UpdatePoolStatus,
+                RaydiumClmmInstruction::UpdateRewardInfos => UpdateRewardInfos,
+            )
+        })();
+
+        let mut decoded_instructions = Vec::new();
+        if let Some(decoded_instruction) = decoded_instruction {
+            decoded_instructions.push(decoded_instruction);
+        }
+
+        if let Some(data) = CpiEvent::decode(&instruction.data) {
+            decoded_instructions.push(RaydiumClmmInstruction::CpiEvent {
+                program_id: PROGRAM_ID,
+                data,
+                accounts: CpiEvent::arrange_accounts(&instruction.accounts),
+            });
+        }
+
+        for payload in metadata.program_data_log_payloads() {
+            if let Some(data) = CpiEvent::decode(payload.as_slice()) {
+                decoded_instructions.push(RaydiumClmmInstruction::CpiEvent {
+                    program_id: PROGRAM_ID,
+                    data,
+                    accounts: None,
+                });
+            }
+        }
+
+        decoded_instructions
     }
 }
